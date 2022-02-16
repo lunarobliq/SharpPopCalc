1ad7df6640a4d8e2a3aece83a89c599e13b71114
ceramicskate0 <64501203+ceramic-skate0@users.noreply.github.com>
2022-02-16 14:57:19 -0500

    Add files via upload

---
+++ PopCalcCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopCalcCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("calc.exe","");
+            startInfo.WorkingDirectory = Directory.GetCurrentDirectory();
+            startInfo.RedirectStandardOutput = false;
+            startInfo.RedirectStandardError = false;
+            startInfo.LoadUserProfile = true;
+            startInfo.UseShellExecute = false;
+            startInfo.CreateNoWindow = false;
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
